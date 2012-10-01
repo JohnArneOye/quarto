4918eaae10d2c4404efd35cc5f288f5d2cf8e26e
John Arne Øye <johnarneoye@gmail.com>
2012-10-01 16:04:34 +0200

    stuff

--- Game.py
+++ Game.py
@@ -5,14 +5,21 @@ class Game:
     #represents the boardstate using bitstrings
     
     def __init__(self, board=[]):
+        #Initiate the board with empty spaces
         self.board = board
         self.board.append([0b00000000, 0b00000000, 0b00000000, 0b00000000])
         self.board.append([0b00000000, 0b00000000, 0b00000000, 0b00000000])
         self.board.append([0b00000000, 0b00000000, 0b00000000, 0b00000000])
         self.board.append([0b00000000, 0b00000000, 0b00000000, 0b00000000])
+        
+        self.remaining_pieces = PIECES.keys()
+        
         self.horizontal_win = False
         self.vertical_win = False
         self.diagonal_win = False
+        
+        self.current_player = 1
+        
         
     def print_board(self):
         print "  1   2   3   4" #x pos
@@ -35,10 +42,10 @@ class Game:
                 return i+1
         #check diagonal lines!
         if (self.board[0][0] & self.board[1][1] & self.board[2][2] & self.board[3][3]) != 0:
-            diagonal_win = True
+            self.diagonal_win = True
             return 1
         elif (self.board[0][3] & self.board[1][2] & self.board[2][1] & self.board[3][0]) != 0:
-            diagonal_win = True
+            self.diagonal_win = True
             return 4
         return False
             
@@ -49,6 +56,61 @@ class Game:
             return True
         return False
             
+            
+import random
+
+class Player:
+    #Abstract player class
+    def __init__(self, game):
+        self.game = game
+        
+    def select_piece(self):
+        return 0
+    
+    def select_position(self):
+        return [0,0]
+
+class HumanPlayer(Player):
+    #Plays based on input through console
+    def __init__(self, game):
+        self.game = game
+        
+    def select_piece(self):
+        return 0
+
+class RandomPlayer(Player):
+    #Plays randomly all the way!
+    #Humpty Dumpty sat on a wall!
+    def __init__(self, game):
+        self.game = game
+        
+    def select_piece(self):
+        pieces = PIECES.keys()
+        return pieces[random.randrange(0, pieces)]
+        
+    def select_position(self):
+        
+
+class NovicePlayer(Player):
+    #Plays randomly
+    #Avoids selecting pieces which will give a win to opponent
+    #Puts a piece if winning position if possible
+    
+class Minimax3Player(Player):
+    #Searches through the tree of game states with a depth of 3
+    #chooses winning move if possible
+    
+class Minimax4Player(Player):
+    #Searches through the tree of game states with a depth of 4
+    #chooses winning move if possible
+    
+class MonteCarloPlayer(Player):
+    #for each possible piece selection/placing given a game state:
+    #simulates 1000k rounds of game
+    #chooses the move which statistically has highest probability of winning
+    
+    
+
 #pieces unstarred.starred unbracketed.bracketed small.big red.blue
 PIECES = {
           0b00000000: '    ',
