e4363d815d19868387d58a2b90104edb8c9a2880
John Arne Øye <johnarneoye@gmail.com>
2012-10-18 17:00:20 +0200

    stuuuuff

--- TCP.py
+++ TCP.py
@@ -9,21 +9,22 @@ class TCP:
         self.s.connect((self.ip, self.port))
     
     #sends a string of information regarding the position selected by our agent
-    def send_position(self, position):
-        self.s.send("1 "+ str(position))
+    def send_position(self, position_index):
+        self.s.send(tr(position_index))
         return False
     
     #sends a string of inforamtino regarding the piece selected by our agent
-    def send_piece(self, piece):
+    def send_piece(self, piece_index):
+        self.s.send(str(piece_index))
         return False
     
     #receives a string of information regarding the position selected by a remote agent
     def receive_position(self):
-        return False
+        return self.s.recv(1024)
     
     #receives a string of informatino regarding the piece selected by a remote agent
     def receive_piece(self):
-        return False
+        return self.s.recv(1024)
  
 #3 import socket
 #   4 
